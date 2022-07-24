67ef71bd49669090bdd1be47594b6dcc71de4bd6
NATILEMMA <natilemma5@gmail.com>
2022-07-24 13:19:49 +0300

    Real estate app

---
+++ demo_app/demo_app/doctype/new_demo_doctype/test_new_demo_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Natnael and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNewdemodoctype(FrappeTestCase):
+	pass
